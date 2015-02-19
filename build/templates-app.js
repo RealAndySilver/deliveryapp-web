a35bd67121140d6ecfbe79e78affb00b9b83d4cb
Carlos Castillo <poleecastillo@gmail.com>
2015-02-19 17:06:51 -0500

    Creada interfaz para Registro de Usuario

--- build/templates-app.js
+++ build/templates-app.js
@@ -9,7 +9,52 @@ angular.module("about/about.tpl.html", []).run(["$templateCache", function($temp
 
 angular.module("createUser/createUser.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("createUser/createUser.tpl.html",
-    "<h1>CreateUser</h1>");
+    "<div layout=\"column\" layout-align=\"center center\" class=\"solo-box\">\n" +
+    "	<md-card class=\"basic-form\">\n" +
+    "		<div>\n" +
+    "			<md-toolbar>\n" +
+    "			    <div class=\"md-toolbar-tools\" layout-align=\"center center\">\n" +
+    "			    	<span class=\"md-flex\">Registro</span>\n" +
+    "			    </div>\n" +
+    "			  </md-toolbar>\n" +
+    "		</div>\n" +
+    "	    <div>\n" +
+    "			<form name=\"loginForm\" ng-submit=\"model.loginUser()\" novalidate>\n" +
+    "				<md-input-container flex>\n" +
+    "					<label>Nombre</label>\n" +
+    "	            	<input name=\"name\" ng-model=\"model.user.name\" required>\n" +
+    "	        	</md-input-container>\n" +
+    "	        	<md-input-container flex>\n" +
+    "					<label>Apellido</label>\n" +
+    "	            	<input name=\"lastname\" ng-model=\"model.lastname\" required>\n" +
+    "	        	</md-input-container>\n" +
+    "				<md-input-container flex>\n" +
+    "					<label>Correo electrónico</label>\n" +
+    "	            	<input name=\"email\" ng-model=\"model.user.email\" required>\n" +
+    "	        	</md-input-container>\n" +
+    "	        	<md-input-container flex>\n" +
+    "					<label>Contraseña</label>\n" +
+    "	            	<input type=\"password\" name=\"password\" ng-model=\"model.user.password\" required>\n" +
+    "	        	</md-input-container>\n" +
+    "	        	<md-input-container flex>\n" +
+    "					<label>Confirmar contraseña</label>\n" +
+    "	            	<input type=\"password\" name=\"password\" ng-model=\"model.user.password\" required>\n" +
+    "	        	</md-input-container>\n" +
+    "	        	<md-input-container flex>\n" +
+    "					<label>Celular</label>\n" +
+    "	            	<input type=\"password\" name=\"password\" ng-model=\"model.user.password\" required>\n" +
+    "	        	</md-input-container>\n" +
+    "	        	<md-input-container flex>\n" +
+    "	        		<md-button class=\"md-raised\">Ingresar</md-button>\n" +
+    "	        	</md-input-container>\n" +
+    "			</form>\n" +
+    "	    </div>\n" +
+    "	    <md-divider></md-divider>\n" +
+    "	    <md-button class=\"md-primary v-margin-btn\">¿Olvidaste tu contraseña?</md-button>\n" +
+    "	    <md-divider></md-divider>\n" +
+    "	    <md-button class=\"md-raised v-margin-btn\" href=\"#\">Crear nueva cuenta</md-button>\n" +
+    "	</md-card>\n" +
+    "</div>");
 }]);
 
 angular.module("home/home.tpl.html", []).run(["$templateCache", function($templateCache) {
