74f3348e773295225ad771f08422f8cfc996b9b1
Carlos Castillo <poleecastillo@gmail.com>
2015-02-25 12:50:30 -0500

    BackUp para ajustes de compatibilidad en Safari.

--- build/src/app/app.js
+++ build/src/app/app.js
@@ -7,36 +7,6 @@
     app.run(function() {});
 
     app.controller('AppController', function($scope) {
-        $scope.$watch('lat', onChange);
-        $scope.$watch('lng', onChange);
-
-        $scope.ome = 'location';
-
-        $scope.lat = 40.730885;
-        $scope.lng = -73.997383;
-
-        onChange();
-
-        function onChange() {
-            var geocoder = new google.maps.Geocoder();
-            var latlng = new google.maps.LatLng($scope.lat, $scope.lng);
-            geocoder.geocode({
-                'latLng': latlng
-            }, function(results, status) {
-                console.log('results', results);
-                console.log('status', status);
-                if (status == google.maps.GeocoderStatus.OK) {
-                    if (results[0]) {
-                        $scope.ome = results[0].formatted_address;
-                    } else {
-                        $scope.ome = 'Location not found';
-                    }
-                } else {
-                    $scope.ome = 'Geocoder failed due to: ' + status;
-                }
-                console.log('ome', $scope.ome);
-            });
-        }
 
     });
 
--- src/app/requestMessenger/requestMessenger.js
+++ src/app/requestMessenger/requestMessenger.js
@@ -1,12 +1,12 @@
 (function(module) {
 
-	module.controller('RequestMessengerController', function() {
+	module.controller('RequestMessengerController', ['$scope', function($scope) {
 		var model = this;
 
 		init();
 
 		function init() {
-			/*var x = document.getElementById("geo-cositas");
+			var x = document.getElementById("geo-cositas");
 
 			model.getLocation = function() {
 				if (navigator.geolocation) {
@@ -19,12 +19,43 @@
 			model.showPosition = function(position) {
 				x.innerHTML = "Latitude: " + position.coords.latitude +
 					"<br>Longitude: " + position.coords.longitude;
-			};*/
+			};
+
+			$scope.$watch('lat', onChange);
+			$scope.$watch('lng', onChange);
+
+			$scope.ome = 'location';
+
+			$scope.lat = 40.730885;
+			$scope.lng = -73.997383;
+
+			onChange();
+
+			function onChange() {
+				var geocoder = new google.maps.Geocoder();
+				var latlng = new google.maps.LatLng($scope.lat, $scope.lng);
+				geocoder.geocode({
+					'latLng': latlng
+				}, function(results, status) {
+					console.log('results', results);
+					console.log('status', status);
+					if (status == google.maps.GeocoderStatus.OK) {
+						if (results[0]) {
+							$scope.ome = results[0].formatted_address;
+						} else {
+							$scope.ome = 'Location not found';
+						}
+					} else {
+						$scope.ome = 'Geocoder failed due to: ' + status;
+					}
+					console.log('ome', $scope.ome);
+				});
+			}
 
 			model.requestMessenger = function() {
 				console.log('objeto servicio ', model.user);
 			};
 		}
-	});
+	}]);
 
 }(angular.module("appMensajeria.requestMessenger")));
