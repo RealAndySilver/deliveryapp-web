faac79d6b88fe7be5ca52ccac0b62cc023742c8f
Carlos Castillo Sánchez <poleecastillo@gmail.com>
2015-04-14 20:52:39 -0500

    Creada interfaz de menú principal

--- build/src/app/recoverPassword/recoverPassword.module.js
+++ build/src/app/recoverPassword/recoverPassword.module.js
@@ -1,6 +1,6 @@
 (function(module) {
 
-    module.config(function ($stateProvider) {
+    module.config(['$stateProvider', function ($stateProvider) {
         $stateProvider.state('recoverPassword', {
             url: '/recoverpassword',
             views: {
@@ -11,7 +11,26 @@
             },
             data:{ pageTitle: 'RecoverPassword' }
         });
-    });
+    }]);
+
+}(angular.module("appMensajeria.recoverPassword", [
+    'ui.router'
+])));
+
+(function(module) {
+
+    module.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('recoverPassword', {
+            url: '/recoverpassword',
+            views: {
+                "main": {
+                    controller: 'RecoverPasswordController as model',
+                    templateUrl: 'recoverPassword/recoverPassword.tpl.html'
+                }
+            },
+            data:{ pageTitle: 'RecoverPassword' }
+        });
+    }]);
 
 }(angular.module("appMensajeria.recoverPassword", [
     'ui.router'
--- build/src/app/searchingMessenger/searchingMessenger.module.js
+++ build/src/app/searchingMessenger/searchingMessenger.module.js
@@ -1,6 +1,6 @@
 (function(module) {
 
-    module.config(function ($stateProvider) {
+    module.config(['$stateProvider', function ($stateProvider) {
         $stateProvider.state('searchingMessenger', {
             url: '/searchingmessenger/:delivery_id',
             views: {
@@ -11,7 +11,26 @@
             },
             data:{ pageTitle: 'SearchingMessenger' }
         });
-    });
+    }]);
+
+}(angular.module("appMensajeria.searchingMessenger", [
+    'ui.router'
+])));
+
+(function(module) {
+
+    module.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('searchingMessenger', {
+            url: '/searchingmessenger/:delivery_id',
+            views: {
+                "main": {
+                    controller: 'SearchingMessengerController as model',
+                    templateUrl: 'searchingMessenger/searchingMessenger.tpl.html'
+                }
+            },
+            data:{ pageTitle: 'SearchingMessenger' }
+        });
+    }]);
 
 }(angular.module("appMensajeria.searchingMessenger", [
     'ui.router'
