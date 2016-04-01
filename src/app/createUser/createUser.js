34a14b33a786f5a19b229ac4241808392e3611fb
Carlos Castillo <poleecastillo@gmail.com>
2016-04-01 10:29:27 -0500

    Correción en función de crear usuario

--- src/app/createUser/createUser.js
+++ src/app/createUser/createUser.js
@@ -8,26 +8,24 @@
 		function init() {
 			model.createUser = function() {
 
-				if ($scope.registerForm.$valid) {
-					AlertsService.loading();
-					console.log('crear home ', model.user);
+				//if ($scope.registerForm.$valid) {
+					//AlertsService.loading();
+					console.log('crear user ', model.user);
 					CreateUserService.createUser(model.user.name, model.user.lastname, model.user.email, model.user.password, model.user.mobilephone, function(response) {
 						console.log(response);
-						AlertsService.cancel();
+						//AlertsService.cancel();
 						if (response.response) {
-							AlertsService.showAlert("Usuario creado Correctamente", "goHome");
+							//AlertsService.showAlert("Usuario creado Correctamente", "goHome");
+							alert('Usuario creado Correctamente');
 						} else {
-							AlertsService.showAlert(response.msg, "");
+							//AlertsService.showAlert(response.msg, "");
+							alert('Ha ocurrido un error');
 						}
 					});
-				} else {
+				/*} else {
 					AlertsService.showSimpleAlert("Completa todos los campos por favor.");
-				}
+				}*/
 
-			};
-
-			model.cancel = function() {
-				$state.reload();
 			};
 		}
 	}]);
